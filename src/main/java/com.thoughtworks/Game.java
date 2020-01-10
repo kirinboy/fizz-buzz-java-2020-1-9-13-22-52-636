1eb546a0ad90ad088cdbdc1abed158bfefe9f968
Kirin <kirinboy@gmail.com>
2020-01-11 00:05:45 +0800

    fix capital

--- src/main/java/com.thoughtworks/Game.java
+++ src/main/java/com.thoughtworks/Game.java
@@ -3,9 +3,9 @@ package com.thoughtworks;
 public class Game {
     public String fizzBuzz(int number) {
         if (number % 3 == 0)
-            return "fizz";
+            return "Fizz";
         if (number % 5 == 0)
-            return "buzz";
-        return "whizz";
+            return "Buzz";
+        return "Whizz";
     }
 }
--- src/test/java/com/thoughtworks/GameTest.java
+++ src/test/java/com/thoughtworks/GameTest.java
@@ -7,31 +7,31 @@ import static org.junit.Assert.assertEquals;
 public class GameTest {
     @Test
     public void should_return_fizz_for_3() {
-        assertEquals("fizz", new Game().fizzBuzz(3));
+        assertEquals("Fizz", new Game().fizzBuzz(3));
     }
 
     @Test
     public void should_return_fizz_for_6() {
-        assertEquals("fizz", new Game().fizzBuzz(6));
+        assertEquals("Fizz", new Game().fizzBuzz(6));
     }
 
     @Test
     public void should_return_buzz_for_5() {
-        assertEquals("buzz", new Game().fizzBuzz(5));
+        assertEquals("Buzz", new Game().fizzBuzz(5));
     }
 
     @Test
     public void should_return_buzz_for_10() {
-        assertEquals("buzz", new Game().fizzBuzz(5));
+        assertEquals("Buzz", new Game().fizzBuzz(5));
     }
 
     @Test
     public void should_return_whizz_for_7() {
-        assertEquals("whizz", new Game().fizzBuzz(7));
+        assertEquals("Whizz", new Game().fizzBuzz(7));
     }
 
     @Test
     public void should_return_whizz_for_14() {
-        assertEquals("whizz", new Game().fizzBuzz(14));
+        assertEquals("Whizz", new Game().fizzBuzz(14));
     }
 }
