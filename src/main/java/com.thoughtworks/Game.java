79c0e677c89b21ddba021e9a0e5dc02586bb336a
Kirin <kirinboy@gmail.com>
2020-01-11 15:56:40 +0800

    should return fizz for 13

--- src/main/java/com.thoughtworks/Game.java
+++ src/main/java/com.thoughtworks/Game.java
@@ -2,6 +2,8 @@ package com.thoughtworks;
 
 public class Game {
     public String fizzBuzz(int number) {
+        if (Integer.toString(number).contains("3"))
+            return "Fizz";
         if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0)
             return "FizzBuzzWhizz";
         if (number % 3 == 0 && number % 5 == 0)
---
+++ src/test/java/com/thoughtworks/when_number_contains_character_3.java
@@ -0,0 +1,12 @@
+package com.thoughtworks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class when_number_contains_character_3 {
+    @Test
+    public void should_fizz_for_13() {
+        assertEquals("Fizz", new Game().fizzBuzz(13));
+    }
+}
