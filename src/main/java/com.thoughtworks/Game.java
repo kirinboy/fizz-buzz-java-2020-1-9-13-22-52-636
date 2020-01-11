8d6f0471752b76b70649bc5f06e5b97355407f31
Kirin <kirinboy@gmail.com>
2020-01-11 16:24:37 +0800

    should return buzzwhizz for 35

--- src/main/java/com.thoughtworks/Game.java
+++ src/main/java/com.thoughtworks/Game.java
@@ -3,10 +3,13 @@ package com.thoughtworks;
 public class Game {
     public String fizzBuzz(int number) {
         String numberString = Integer.toString(number);
+        if (numberString.contains("5")){
+            if (number % 7 == 0)
+                return "BuzzWhizz";
+            return "Buzz";
+        }
         if (numberString.contains("3"))
             return "Fizz";
-        if (numberString.contains("5"))
-            return "Buzz";
         if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0)
             return "FizzBuzzWhizz";
         if (number % 3 == 0 && number % 5 == 0)
--- src/test/java/com/thoughtworks/when_number_contains_character_5.java
+++ src/test/java/com/thoughtworks/when_number_contains_character_5.java
@@ -9,4 +9,9 @@ public class when_number_contains_character_5 {
     public void should_return_buzz_for_15() {
         assertEquals("Buzz", new Game().fizzBuzz(15));
     }
+
+    @Test
+    public void should_return_buzzwhizz_for_35() {
+        assertEquals("BuzzWhizz", new Game().fizzBuzz(35));
+    }
 }
