0136769c5f21a232182cca3925467aeb72c1f223
Kirin <kirinboy@gmail.com>
2020-01-11 15:49:05 +0800

    should return buzzwhizz for 140

--- src/main/java/com.thoughtworks/Game.java
+++ src/main/java/com.thoughtworks/Game.java
@@ -4,6 +4,8 @@ public class Game {
     public String fizzBuzz(int number) {
         if (number % 3 == 0 && number % 5 == 0)
             return "FizzBuzz";
+        if (number % 5 == 0 && number % 7 == 0)
+            return "BuzzWhizz";
         if (number % 3 == 0)
             return "Fizz";
         if (number % 5 == 0)
--- src/test/java/com/thoughtworks/when_number_is_a_multiple_of_two_numbers_of_3_5_7.java
+++ src/test/java/com/thoughtworks/when_number_is_a_multiple_of_two_numbers_of_3_5_7.java
@@ -9,4 +9,9 @@ public class when_number_is_a_multiple_of_two_numbers_of_3_5_7 {
     public void should_fizzbuzz_for_60() {
         assertEquals("FizzBuzz", new Game().fizzBuzz(60));
     }
+
+    @Test
+    public void should_buzzwhizz_for_140() {
+        assertEquals("BuzzWhizz", new Game().fizzBuzz(140));
+    }
 }
